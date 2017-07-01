b7eb14ab75d86638a705cab5489db75d3d12c0e5
Qudrat Abdurahimov <dasturchi@mail.ru>
2017-07-01 22:37:08 +0500

    String2

---
+++ src/String2.java
@@ -0,0 +1,11 @@
+/**
+ * String2.
+ * Given an integer N (32 ? N ? 126), output a character with the numeric value N in the character set.
+ *
+ * Дано целое число N (32 ? N ? 126). Вывести символ с кодом, равным N.
+ *
+ * Created by Qudrat Abdurahimov on 01.07.2017.
+ * http://dastur.uz; dasturchi@mail.ru
+ */
+public class String2 {
+}
