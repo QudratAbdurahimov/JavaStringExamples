28cfb8a452f178f80e2195039a586be6fca9db44
Qudrat Abdurahimov <dasturchi@mail.ru>
2017-07-01 22:09:39 +0500

    String1

---
+++ src/String1.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * Given a character C, output its numeric value in the character set.
+ *
+ * Дан символ C. Вывести его код (т. е. номер в кодовой таблице).
+ *
+ * Created by Qudrat Abdurahimov on 01.07.2017.
+ * http://dastur.uz; dasturchi@mail.ru
+ */
+public class String1 {
+    public static void main(String[] args) throws IOException {
+        char c;
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Input character");
+
+        c = (char) br.read();
+        System.out.println(c + "->" + (int) c);
+
+    }
+}
